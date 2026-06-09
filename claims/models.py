c45589dd9b07457eef40177a3b5e7af1506b1e6a
krushnaraj <krushnarajjadhav2003@gmail.com>
2026-06-09 10:28:43 +0530

    9june2026

--- claims/models.py
+++ claims/models.py
@@ -182,3 +182,159 @@ class Total_Charges(models.Model):
 
     class Meta:
         db_table = "total_charges"
+
+
+class EDIEMP(models.Model):
+
+    TECLNT = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    TESEQ = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    TESSN = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    TEDSSN = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    TENAME = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    TEDOB = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "ediemp"
+
+
+class DEPNP(models.Model):
+
+    DPDROP = models.CharField(max_length=255, null=True, blank=True)
+    DPCLNT = models.CharField(max_length=255, null=True, blank=True)
+    DPSSN = models.CharField(max_length=255, null=True, blank=True)
+    DPSEQ = models.CharField(max_length=255, null=True, blank=True)
+    DPNAME = models.CharField(max_length=255, null=True, blank=True)
+    DPLNAM = models.CharField(max_length=255, null=True, blank=True)
+    DPDOBY = models.CharField(max_length=255, null=True, blank=True)
+    DPDOBM = models.CharField(max_length=255, null=True, blank=True)
+    DPDOBD = models.CharField(max_length=255, null=True, blank=True)
+    DPDODY = models.CharField(max_length=255, null=True, blank=True)
+    DPDODM = models.CharField(max_length=255, null=True, blank=True)
+    DPDODD = models.CharField(max_length=255, null=True, blank=True)
+    DPDSSN = models.CharField(max_length=255, null=True, blank=True)
+    DPSEX = models.CharField(max_length=255, null=True, blank=True)
+    DPTYPE = models.CharField(max_length=255, null=True, blank=True)
+    DPRLTN = models.CharField(max_length=255, null=True, blank=True)
+    DPEFDY = models.CharField(max_length=255, null=True, blank=True)
+    DPEFDM = models.CharField(max_length=255, null=True, blank=True)
+    DPEFDD = models.CharField(max_length=255, null=True, blank=True)
+    DPTDTY = models.CharField(max_length=255, null=True, blank=True)
+    DPTDTM = models.CharField(max_length=255, null=True, blank=True)
+    DPTDTD = models.CharField(max_length=255, null=True, blank=True)
+    DPCOB = models.CharField(max_length=255, null=True, blank=True)
+    DPPLAN = models.CharField(max_length=255, null=True, blank=True)
+    DPCLAS = models.CharField(max_length=255, null=True, blank=True)
+    DPSTAT = models.CharField(max_length=255, null=True, blank=True)
+    DPALTP = models.CharField(max_length=255, null=True, blank=True)
+    DPSELF = models.CharField(max_length=255, null=True, blank=True)
+    DPMDFL = models.CharField(max_length=255, null=True, blank=True)
+    DPMEFY = models.CharField(max_length=255, null=True, blank=True)
+    DPMEFM = models.CharField(max_length=255, null=True, blank=True)
+    DPMEFD = models.CharField(max_length=255, null=True, blank=True)
+    DPMGYY = models.CharField(max_length=255, null=True, blank=True)
+    DPMGMM = models.CharField(max_length=255, null=True, blank=True)
+    DPMGDD = models.CharField(max_length=255, null=True, blank=True)
+    DPDVYY = models.CharField(max_length=255, null=True, blank=True)
+    DPDVMM = models.CharField(max_length=255, null=True, blank=True)
+    DPDVDD = models.CharField(max_length=255, null=True, blank=True)
+    DPBSFL = models.CharField(max_length=255, null=True, blank=True)
+    DPDSFL = models.CharField(max_length=255, null=True, blank=True)
+    DPMGFL = models.CharField(max_length=255, null=True, blank=True)
+    DPDVFL = models.CharField(max_length=255, null=True, blank=True)
+    DPCRYY = models.CharField(max_length=255, null=True, blank=True)
+    DPCRMM = models.CharField(max_length=255, null=True, blank=True)
+    DPCRDD = models.CharField(max_length=255, null=True, blank=True)
+    DPUPYY = models.CharField(max_length=255, null=True, blank=True)
+    DPUPMM = models.CharField(max_length=255, null=True, blank=True)
+    DPUPDD = models.CharField(max_length=255, null=True, blank=True)
+    DPUSER = models.CharField(max_length=255, null=True, blank=True)
+    DPCBVY = models.CharField(max_length=255, null=True, blank=True)
+    DPCBVM = models.CharField(max_length=255, null=True, blank=True)
+    DPHICN = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        db_table = "depnp"
+
+
+class PROVP(models.Model):
+
+    PRSEQ = models.CharField(
+        db_column="PRSEQ#",
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRBSEQ = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRPNAM = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRADR1 = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRCITY = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRST = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRZIP5 = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    PRNUM = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "provp"
