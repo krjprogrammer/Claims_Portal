f06d8197b40d8fd2ef6edff6fb29f6cca606db2a
krushnaraj <krushnarajjadhav2003@gmail.com>
2026-06-29 16:18:16 +0530

    new_changes2

--- claims/models.py
+++ claims/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 class Transaction(models.Model):
     filename = models.CharField(max_length=500)
     filetype = models.CharField(max_length=1)
@@ -406,7 +405,7 @@ class PortalRoles(models.Model):
 class PortalUser(AbstractUser):
     role = models.ForeignKey(PortalRoles, on_delete=models.SET_NULL, null=True, blank=True)
     status = models.BooleanField(default=True)
-    temp_password = models.CharField(max_length=10,null=True,blank=True)
+    temp_password = models.CharField(max_length=30,null=True,blank=True)
     totp_secret = models.CharField(max_length=64, null=True, blank=True)
     totp_enabled = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
